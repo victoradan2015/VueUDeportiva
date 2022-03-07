0352d2481011e7f492847e4a90148dc39bd72e8e
victoradan2015 <victoradan2015@gmail.com>
2022-03-07 15:02:24 -0600

    .
    commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <registro/>
+    <login/>
+    <navbar/>
+  </div>
+</template>
+
+<script>
+import Registro from './components/sesion/Registro.vue';
+import Login from './components/sesion/Login.vue';
+import Navbar from './components/Navbar.vue';
+
+export default {
+  name: "App",
+  components: {
+    Registro,
+    Login,
+    Navbar
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="Navbar">
+    <h1>{{ msg }}</h1>
+
+    <h1 v-if="awesome">Vue is awesome!</h1>
+    
+    <h3>Condicionales v-if v-else, v-for</h3>
+
+    <div v-if="Math.random() > 0.5">
+      Ahora puedes verme
+    </div>
+    <div v-else>
+      Ahora no puedes verme
+    </div> 
+
+
+    <h3>Installed CLI Plugins</h3>
+
+
+    
+  </div>
+</template>
+
+<script>
+import './assets/tailwind.css';
+
+export default {
+  name: 'HolaMundo',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
